5820358bbb7b41803eca823475d6e29a53e32571
oosayeroo <98313914+oosayeroo@users.noreply.github.com>
2024-05-11 19:01:11 +0100

    Add files via upload

---
+++ sayer-props/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'oosayeroo' 
+description 'sayer-props'
+version '1.0.0'
+
+files {
+    'stream/*.ytyp',
+    -- 'stream/sayer_shell1.ytyp',
+    -- 'stream/firepot.ytyp',
+}
+data_file 'DLC_ITYP_REQUEST' 'stream/*.ytyp'
+-- data_file 'DLC_ITYP_REQUEST' 'stream/sayer_shell1.ytyp'
---
+++ sayer-props/read.md
@@ -0,0 +1,7 @@
+SAYER CHEM CRATE
+
+only the ydr and ytyp in the stream folder is active. 
+- clean folder contains prop without dirt
+- alt_dirty contains prop with dirt
+
+- copy whichever you want into the stream folder to use that variant
